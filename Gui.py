0fe1d397ce693dca715da001024010cbab9f83a9
Timon <timon-schule@online.de>
2022-04-09 18:40:46 +0200

    1. Grobe Struktur für das Erstellen der Deutschlandkarte + Streckennetz 2. Funktion hinzugefügt, die txt-Dateien in .json Files umwandeln kann

--- Gui.py
+++ Gui.py
@@ -1,34 +1,75 @@
 import pandas
 import sys
+import json
 from PySide6 import QtCore
 from PySide6 import QtWidgets
 from PySide6 import QtGui
 
-#stops_longdistance = pandas.read_csv('stops.csv')
+# Funktion, der eine txt Datei übergeben werden kann und die daraus eine JSON Datei (nur mit den Koordinaten) macht
+def convert_txt_to_json(filename):
+    stops_longdistance = pandas.read_csv(filename)
+    coordinates = stops_longdistance.iloc[:,[2,3]]
+    coordinates_json = coordinates.to_json(orient = 'split')
 
-#coordinates = stops_longdistance.iloc[:,[2,3]]
+    return coordinates_json
 
-#print(coordinates)
-###########
-
+# Klasse um das Main Window zu erstellen
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Hier gehts weiter, jetzt muss die Karte gemalt werden
+        # Hier müssen die Koordinaten geändert werden
+        scene = QtWidgets.QGraphicsScene()#-180, -90, 360, 180
+
+######## Experiment
+
+       # x = convert_txt_to_json('stops.txt')
+       # print(x)
+
+       #stations_data = convert_txt_to_json('stops.txt')
+       #for index, points in stations_data.items():
+    
+
+###################
+
+        # Hier gehts weiter, jetzt muss die Karte gemalt werden (Koordinaten für die Deutschlandkarte werden benötigt)
         map_data = self.load_map_data()
-        
+        for bundesland, polygons in map_data.items():
+            for polygon in polygons:
+                qpolygon = QtGui.QPolygonF()
+                for x, y in polygon:
+                    qpolygon.append(QtCore.QPointF(x, y))
+                scene.addPolygon(qpolygon)
+
+        germany_map = QtWidgets.QGraphicsView()
+        germany_map.setScene(scene)
+        germany_map.scale(1, -1)
+
+######################
+
+        # Layout gestalten
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(germany_map)
+
+        window_content = QtWidgets.QWidget()
+        window_content.setLayout(layout)
+        self.setCentralWidget(window_content)
 
 
+    # Hier soll später die JSON Datei mit der Deutschlandkarte geladen werden
     def load_map_data(self):
-        with open('stops.txt') as file:
-            stops_longdistance = pandas.read_csv('stops.txt')
-            print(stops_longdistance)
-        return stops_longdistance
+        with open('germany.json') as file:
+            
+            germany_coordinates = json.load(file)
+
+        return germany_coordinates
 
 
 
+  
 
+
+# Aufruf der Main Window Klasse und darstellen des Fensters
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
