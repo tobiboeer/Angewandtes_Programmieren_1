1b88d4c5f1cbb5ab735343aaf7b84334be096b92
Timon <timon-schule@online.de>
2022-04-26 12:06:03 +0200

    PySide2 in PySide 6 geändert, sonst wäre das eine veraltete Version

--- sideWindow.py
+++ sideWindow.py
@@ -1,9 +1,9 @@
 import sys
 import json
 import pandas
-from PySide2 import QtCore
-from PySide2 import QtWidgets
-from PySide2 import QtGui
+from PySide6 import QtCore
+from PySide6 import QtWidgets
+from PySide6 import QtGui
 
 
 class sideWindow(QtWidgets.QMainWindow):
