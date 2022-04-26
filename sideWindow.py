b810b882638c6976f8d0a71cc23f3578b48bfdda
Tobias <tobias.boeer@student.jade-hs.de>
2022-04-26 20:28:49 +0200

    Update SideWindow
    habe das Layout verbessert.
    Nun sind Textfelder mit drin.
    Weiterhin soll eine Tabelle der Bahnhöfe verknüpft werden und die Gui sollte interaktiv sein.

--- sideWindow.py
+++ sideWindow.py
@@ -5,18 +5,58 @@ from PySide6 import QtCore
 from PySide6 import QtWidgets
 from PySide6 import QtGui
 
-
-class sideWindow(QtWidgets.QMainWindow):
+#class tableModel(QtCore.QAbstractTableModel):
+ #   """ Hier sollen die Tabellen mit den Ankunfts- und Abfahrtsbahnhöfen eingelesen und dargestellt werden."""
+  #  def __init__(self):
+   #     """ Notwendige Konstruktorfunktion."""
+    #    super().__init__()
+     #   
+      #  """ Zuweisen des verschiedenen Bahnverkehrs und einlesen der Daten. Über[stop_name] kommt man
+       # an die verschiedenen Haltestellen."""
+        #stops_schienenverkehr = pandas.read_csv("stops_schienenverkehr.csv")
+        #stops_schienenregionalverkehr = pandas.read_csv("stops_schienenregionalverkehr.csv")
+#        stops_public_traffic = pandas.read_csv("stops_oeffentlicher_Nahverkehr.csv")
+ #       
+  #  def rowCount(self,parent=None):
+   #     return 100
+    #    
+#    def columnCount(self, parent=None):
+ #       return 2
+  #  
+   # def data(self,index, role = QtCore.Qt.DisplayRole):
+    #    if role != QtCore.Qt.DisplayRole:
+     #       return None
+      #  
+       # stations = self.stops_public_traffic.iloc[index.row(), index.column()]
+        #return str(stations)    
+    
+    
+class interactivePlaner(QtWidgets.QMainWindow):
     """ Klasse für ein Seitliches Fensterbild, um die Route interaktiv zu machen."""
+    
     def __init__(self):
         """ Konstruktorklasse, die in der Klasse vorhanden sein muss."""
+        
         super().__init__()
         
         """ Deklarieren eines Layouts."""
         layout = QtWidgets.QVBoxLayout()
-                
+        
+        """ Deklarieren eines Gruppenlayouts. funktion noch nicht verstanden.."""
+        group = QtWidgets.QGroupBox("Routenplaner")
+        
         """ Deklarieren der Textfelder und Label für die interaktive GUI und anhängen der Widgets an
         das Layout über 'addWidget'."""
+        textfield_start = QtWidgets.QTextEdit()
+        label_textfield_start = QtWidgets.QLabel("Abfahrbahnhof:")
+        layout.addWidget(label_textfield_start) 
+        layout.addWidget(textfield_start) 
+
+        textfield_destination = QtWidgets.QTextEdit()
+        label_textfield_destination = QtWidgets.QLabel("Ankunftbahnhof:")
+        layout.addWidget(label_textfield_destination)
+        layout.addWidget(textfield_destination)
+        
         label_textfield_date = QtWidgets.QLabel("Datum der Abfahrt:")
         textfield_date = QtWidgets.QDateEdit()
         layout.addWidget(label_textfield_date)
@@ -26,55 +66,43 @@ class sideWindow(QtWidgets.QMainWindow):
         textfield_time = QtWidgets.QTimeEdit()
         layout.addWidget(label_textfield_time)
         layout.addWidget(textfield_time)
-
-        textfield_destination = QtWidgets.QWidget()
-        label_textfield_destination = QtWidgets.QLabel("Ankunftbahnhof:")
-        layout.addWidget(label_textfield_destination)
-        layout.addWidget(textfield_destination)
         
-        textfield_start = QtWidgets.QWidget()
-        label_textfield_start = QtWidgets.QLabel("Abfahrbahnhof:")
-        layout.addWidget(label_textfield_start) 
-        layout.addWidget(textfield_start)        
-       
         button_start = QtWidgets.QPushButton("GO!")
         layout.addWidget(button_start)
+        
+        """ Zusammenfassen des Layouts und der Widgets auf dem Fensterinhalt."""
+        window_content = QtWidgets.QWidget()
+        window_content.setLayout(layout)
+        self.setCentralWidget(window_content)
+
+class mainSideWindow(QtWidgets.QMainWindow):
+    """ Idee dieser Klasse ist ein Hauptfenster, welches mit dem oberen Abschnitt die Routenplanung 
+    beinhaltet und auf dem unteren Abschnitt die Tabelle mit den verschiedenen Bahnhöfen darstellt."""
+    
+    def __init__(self):
+        """ Notwendige Konstruktorklasse, die die vorherigen Klassen 'interactive_planer' und 
+        'tableModel' aufruft und auf ein Main Window projiziert."""
+        super().__init__()
+        
+        """ Aufrufen der vorherigen Klassen."""
+        #table_view = tableModel()
+        #table_view = QtWidgets.QWidget()
+        interactive_planer = interactivePlaner()
+        
+        """ Definieren eines Layouts auf dem die vorherigen Klassen projiziert werden."""
+        layout = QtWidgets.QVBoxLayout()
+        #layout.addWidget(table_view)
+        layout.addWidget(interactive_planer)
 
         """ Zusammenfassen des Layouts und der Widgets auf dem Fensterinhalt."""
         window_content = QtWidgets.QWidget()
         window_content.setLayout(layout)
         self.setCentralWidget(window_content)
-        
-        
-        """ Zuweisen des verschiedenen Bahnverkehrs und einlesen der Daten. Über[stop_name] kommt man
-        an die verschiedenen Haltestellen."""
-        stops_schienenverkehr = self.load_stops_schienenverkehr()
-        stops_schienenregionalverkehr = self.load_stops_schienenregionalverkehr()
-        stops_public_traffic = self.load_stops_public_traffic()
-        
-    def load_stops_schienenverkehr(self):
-        """ Laden der Bahnhöfe aus dem Schienenverkehr. """
-        stops = pandas.read_csv("stops_schienenverkehr.csv")  
-        return stops
-        
-    def load_stops_schienenregionalverkehr(self):
-        """ Laden der Bahnhöfe aus dem Schienenregionalverkehr. """
-        stops = pandas.read_csv("stops_schienenregionalverkehr.csv")  
-        return stops
-        
-    def load_stops_public_traffic(self):
-        """ Laden der Bahnhöfe aus dem öffentlichen Nahverkehr. """
-        stops = pandas.read_csv("stops_öffentlicher_Nahverkehr.csv")  
-        return stops
-
-
 
 if __name__ == "__main__":
-    """ Da für eine GUI eine App notwendig ist, 
-    wird diese ersteinmal gebaut. """ 
+    """ Da für eine GUI eine App notwendig ist, wird diese ersteinmal gebaut. """ 
+    
     app = QtWidgets.QApplication(sys.argv)
-    """ Die Aufgabe ist einen Frame zu basteln der Hello World
-    sagt. """
-    window = sideWindow()
+    window = mainSideWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
