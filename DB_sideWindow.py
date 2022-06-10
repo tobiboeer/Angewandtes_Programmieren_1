f6a1f19d26f176e5059a8fe81de20f89df555863
Tobias <tobias.boeer@student.jade-hs.de>
2022-06-10 07:53:54 +0200

    Verbesserung
    test wegen Neukalibrerung Desktop Tool

--- DB_sideWindow.py
+++ DB_sideWindow.py
@@ -24,6 +24,7 @@ Licence:
 """
 
 from PySide6 import QtWidgets
+from PySide6 import QtCore
 from datetime import datetime
 
 class sideWindow(QtWidgets.QMainWindow):
@@ -47,86 +48,29 @@ class sideWindow(QtWidgets.QMainWindow):
         self.main_gui = main_gui
         self.setMinimumSize(250, 450)
         
-        # -------------- COMBOBOXES ----------------
-        self.combobox_start = QtWidgets.QComboBox()
-        self.combobox_start.setPlaceholderText("- Bahnhof wählen -")
-        self.combobox_start.currentTextChanged.connect \
-            (self.change_start_station)
         
-        # -------------- TEXT FIELDS ----------------
-        self.textfield_date = QtWidgets.QDateEdit()
-        start_date = datetime(2022, 0o1, 0o1)
-        end_date = datetime(2022, 12, 31)
-        self.textfield_date.setDateRange(start_date, end_date)
+        #------------------ new -----------------------
+        structurizer = QtWidgets.QTabWidget()
         
-        self.textfield_time = QtWidgets.QTimeEdit()
-        self.textfield_time_dif = QtWidgets.QTimeEdit()
-        self.textfield_allInfo = QtWidgets.QTextEdit()
+        self.information_page = QtWidgets.QWidget()
+        self.set_information_page()
         
-        self.text = ""
-        self.update_text(" ")
-        self.start_station = " "
+        self.set_planning_page()
         
-        # -------------- LABELS  ------------------
-        label_button_traffic_style = QtWidgets.QLabel("Bahnart:")
-        label_combobox_start = QtWidgets.QLabel("Abfahrbahnhof:")
-        label_textfield_date_time = QtWidgets.QLabel \
-            ("Datum und Zeit der Abfahrt:")
-        self.label_textfield_time_dif = QtWidgets.QLabel("Zeitfenster")
-        label_textfield_allInfo = QtWidgets.QLabel \
-            ("Besondere Informationen:")
-        label_button_request = QtWidgets.QLabel \
-            ("Daten zum Bahnhof erstellen:")
-
-        # -------------- BUTTONS ------------------
-        self.button_nahverkehr = QtWidgets.QPushButton("Nahverkehr")
-        self.button_fernverkehr = QtWidgets.QPushButton("Fernverkehr")
-        self.button_regional = QtWidgets.QPushButton("Regional")
-        self.button_request = QtWidgets.QPushButton("Anfrage stellen")
-        self.button_request.setFixedSize(100, 40)
-
-        self.button_fernverkehr.clicked.connect \
-            (self.click_function_long_distance)
-        self.button_nahverkehr.clicked.connect \
-            (self.click_function_short_distance)
-        self.button_regional.clicked.connect \
-            (self.click_function_regional)
-        self.button_request.clicked.connect(self.train_station_request)
-
-        # -------------- LAYOUTS ------------------
-        button_layout_traffic = QtWidgets.QHBoxLayout()
-        button_layout_traffic.addWidget(self.button_nahverkehr)
-        button_layout_traffic.addWidget(self.button_fernverkehr)
-        button_layout_traffic.addWidget(self.button_regional)
+        self.function_page = QtWidgets.QWidget()
+        self.set_extension_page()
         
-        date_time_layout = QtWidgets.QHBoxLayout()
-        date_time_layout.addWidget(self.textfield_date)
-        date_time_layout.addWidget(self.textfield_time)
-
-        time_dif_layout = QtWidgets.QHBoxLayout()
-        time_dif_layout.addWidget(self.textfield_time_dif)
-
-        button_request_layout = QtWidgets.QHBoxLayout()
-        button_request_layout.addWidget(self.button_request)
-
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(label_button_traffic_style)
-        layout.addLayout(button_layout_traffic)
-        layout.addWidget(label_combobox_start) 
-        layout.addWidget(self.combobox_start)
-        layout.addWidget(label_textfield_date_time)
-        layout.addLayout(date_time_layout)
-        layout.addWidget(self.label_textfield_time_dif)
-        layout.addLayout(time_dif_layout)
-        layout.addWidget(label_button_request)
-        layout.addLayout(button_request_layout)
-        layout.addWidget(label_textfield_allInfo)
-        layout.addWidget(self.textfield_allInfo)
         
-        window_content = QtWidgets.QWidget()
-        window_content.setLayout(layout)
-        self.setCentralWidget(window_content)
+        structurizer.addTab(self.information_page, "Auskunft")
+        structurizer.addTab(self.planning_page, "Routenplaner")
+        structurizer.addTab(self.function_page, "Information")
+       
+        self.setCentralWidget(structurizer)
+        #--------------------------------------------
+        
         self.set_train_stations_list()
+
+
 
     def set_train_station(self, new_station):
         """
@@ -227,3 +171,254 @@ class sideWindow(QtWidgets.QMainWindow):
 
         self.main_gui.model.change_train_station_info(time_span, day, \
             hour, minute, self.start_station)
+            
+#--------------------------------new ---------------------------
+    def set_information_page(self):
+        """
+        Sets the conditions of the information page in the tab structurizer.
+        """
+        
+        # -------------- COMBOBOX  ------------------
+        self.combobox_start = QtWidgets.QComboBox()
+        self.combobox_start.setPlaceholderText("- Bahnhof wählen -")
+        
+        # -------------- TEXTFIELD  ------------------
+        self.textfield_date = QtWidgets.QDateEdit()
+        start_date = datetime(2022, 0o1, 0o1)
+        end_date = datetime(2022, 12, 31)
+        self.textfield_date.setDateRange(start_date, end_date)
+        
+        self.textfield_time = QtWidgets.QTimeEdit()
+        self.textfield_time_dif = QtWidgets.QTimeEdit()
+        self.textfield_allInfo = QtWidgets.QTextEdit()
+        
+        # -------------- LABELS  ------------------
+        label_button_traffic_style = QtWidgets.QLabel("Bahnart:")
+        label_combobox_start = QtWidgets.QLabel("Abfahrbahnhof:")
+        label_textfield_date_time = QtWidgets.QLabel \
+            ("Datum und Zeit der Abfahrt:")
+        self.label_textfield_time_dif = QtWidgets.QLabel("Zeitfenster")
+        label_textfield_allInfo = QtWidgets.QLabel \
+            ("Besondere Informationen:")
+        label_button_request = QtWidgets.QLabel \
+            ("Daten zum Bahnhof erstellen:")
+
+        # -------------- BUTTONS ------------------
+        self.button_nahverkehr = QtWidgets.QPushButton("Nahverkehr")
+        self.button_fernverkehr = QtWidgets.QPushButton("Fernverkehr")
+        self.button_regional = QtWidgets.QPushButton("Regional")
+        self.button_request = QtWidgets.QPushButton("Anfrage stellen")
+        self.button_request.setFixedSize(100, 40)
+
+        # -------------- LAYOUTS ------------------
+        button_layout_traffic = QtWidgets.QHBoxLayout()
+        button_layout_traffic.addWidget(self.button_nahverkehr)
+        button_layout_traffic.addWidget(self.button_fernverkehr)
+        button_layout_traffic.addWidget(self.button_regional)
+        
+        date_time_layout = QtWidgets.QHBoxLayout()
+        date_time_layout.addWidget(self.textfield_date)
+        date_time_layout.addWidget(self.textfield_time)
+
+        time_dif_layout = QtWidgets.QHBoxLayout()
+        time_dif_layout.addWidget(self.textfield_time_dif)
+
+        button_request_layout = QtWidgets.QHBoxLayout()
+        button_request_layout.addWidget(self.button_request)
+
+        sublayout = QtWidgets.QVBoxLayout()
+        self.information_page.setLayout(sublayout)
+        sublayout.addWidget(label_button_traffic_style)
+        sublayout.addLayout(button_layout_traffic)
+        sublayout.addWidget(label_combobox_start) 
+        sublayout.addWidget(self.combobox_start)
+        sublayout.addWidget(label_textfield_date_time)
+        sublayout.addLayout(date_time_layout)
+        sublayout.addWidget(self.label_textfield_time_dif)
+        sublayout.addLayout(time_dif_layout)
+        sublayout.addWidget(label_button_request)
+        sublayout.addLayout(button_request_layout)
+        sublayout.addWidget(label_textfield_allInfo)
+        sublayout.addWidget(self.textfield_allInfo)
+        
+        #--------------------- FUNCTIONS -----------
+        self.combobox_start.currentTextChanged.connect \
+            (self.change_start_station)
+            
+        self.text = ""
+        self.update_text(" ")
+        self.start_station = " "
+        
+        self.button_fernverkehr.clicked.connect \
+            (self.click_function_long_distance)
+        self.button_nahverkehr.clicked.connect \
+            (self.click_function_short_distance)
+        self.button_regional.clicked.connect \
+            (self.click_function_regional)
+        self.button_request.clicked.connect \
+            (self.train_station_request)
+    
+    def set_planning_page(self):
+        """
+        sets the Options of the planning page in the tab structurizer.
+        """
+        self.planning_page = QtWidgets.QWidget()
+        
+        #--------------- CHECKBOX -----------------
+        checkbox_nah = QtWidgets.QCheckBox('Nahverkehr')
+        checkbox_fern = QtWidgets.QCheckBox('Fernverkehr')
+        checkbox_regional = QtWidgets.QCheckBox \
+            ('Regionalverkehr')
+        
+        # -------------- COMBOBOX  ------------------
+        self.combobox_begin = QtWidgets.QComboBox()
+        self.combobox_begin.setPlaceholderText \
+            ("---This is where the fun begins---")
+        self.combobox_end = QtWidgets.QComboBox()
+        self.combobox_end.setPlaceholderText \
+            ("---The boy who lived...---")
+        combobox_route = QtWidgets.QComboBox()
+        combobox_route.setPlaceholderText \
+            ("--- Fast and the Furious ---")
+        
+        # -------------- TEXTFIELD  ------------------
+        date_field = QtWidgets.QDateEdit()
+        start_date = datetime(2022, 0o1, 0o1)
+        end_date = datetime(2022, 12, 31)
+        date_field.setDateRange(start_date, end_date)
+        time_field = QtWidgets.QTimeEdit()
+        change_train_time = QtWidgets.QTimeEdit()
+        
+        #--------------- PROGRESSBAR ---------------
+        self.calculation_progressbar = QtWidgets.QProgressBar()
+        self.calculation_progressbar.setRange(0,10000)
+        self.calculation_progressbar.setValue(0)
+        
+        timer_progressbar = QtCore.QTimer(self)
+        timer_progressbar.timeout.connect(self.edit_progressbar)
+        timer_progressbar.start(1000)
+        
+        # -------------- BUTTONS ------------------
+        button_search = QtWidgets.QPushButton('Suchen')
+        button_delete = QtWidgets.QPushButton('Eingabe löschen')        
+        
+        # -------------- LABELS  ------------------
+        combobox_begin_label = QtWidgets.QLabel \
+            ("Abfahrtbahnhof/ -haltestelle:")
+        combobox_end_label = QtWidgets.QLabel \
+            ("Ankunftsbahnhof/ -haltestelle:")
+        datetime_label = QtWidgets.QLabel \
+            ("Datum und Uhrzeit:")
+        change_train_time_label = QtWidgets.QLabel \
+            ("Umstiegszeit:")
+        combobox_route_label = QtWidgets.QLabel \
+            ("Geschwindigkeit der Route:")
+        calculation_progressbar_label = QtWidgets.QLabel \
+            ("Berechnungen:")
+        
+        # -------------- LAYOUT  ------------------
+        checkbox_layout = QtWidgets.QHBoxLayout()
+        checkbox_layout.addWidget(checkbox_nah)
+        checkbox_layout.addWidget(checkbox_fern)
+        checkbox_layout.addWidget(checkbox_regional)
+        
+        combobox_layout = QtWidgets.QVBoxLayout()
+        combobox_layout.addWidget(combobox_begin_label)
+        combobox_layout.addWidget(self.combobox_begin)
+        combobox_layout.addWidget(combobox_end_label)
+        combobox_layout.addWidget(self.combobox_end)
+        
+        datetime_layout = QtWidgets.QHBoxLayout()
+        datetime_layout.addWidget(date_field)
+        datetime_layout.addWidget(time_field)
+        
+        organizing_times_layout = QtWidgets.QVBoxLayout()
+        organizing_times_layout.addWidget(change_train_time_label)
+        organizing_times_layout.addWidget(change_train_time)
+        organizing_times_layout.addWidget(datetime_label)
+        organizing_times_layout.addLayout(datetime_layout)
+        
+        route_layout = QtWidgets.QHBoxLayout()
+        route_layout.addWidget(combobox_route_label)
+        route_layout.addWidget(combobox_route)
+        
+        progress_layout = QtWidgets.QVBoxLayout()
+        progress_layout.addWidget(calculation_progressbar_label)
+        progress_layout.addWidget(self.calculation_progressbar)
+        
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addWidget(button_delete)
+        button_layout.addWidget(button_search)
+        
+        sublayout = QtWidgets.QVBoxLayout()
+        self.planning_page.setLayout(sublayout)
+        sublayout.addLayout(checkbox_layout)
+        sublayout.addLayout(combobox_layout)
+        sublayout.addLayout(organizing_times_layout)
+        sublayout.addLayout(route_layout)
+        sublayout.addLayout(progress_layout)
+        sublayout.addLayout(button_layout)
+        
+        #--------------------- FUNCTIONS -----------
+        # Wenn die Checkboxen geklickt werden, soll sich die jeweilige Liste
+        # um die angeklickte Version erweitern.
+        
+        # Die erstellte Liste wird in den Comboboxen angezeigt.
+        
+        # Die Wahl der Geschwindigkeit hat einen Einfluss auf die Route.
+        
+        # Wenn eine Haltestelle ausgewählt ist, soll der Verlauf in der Tabelle
+        # angezeigt werden.
+        
+        # Die Zeit, die für die Berechnung verwendet wird, soll in der
+        # Progress Bar als Prozent angegeben werden.
+        
+        
+        self.combobox_begin.currentTextChanged.connect \
+            (self.change_start_station)
+            
+        self.combobox_end.currentTextChanged.connect \
+            (self.change_start_station)
+        
+        combobox_route.addItem("Kurz")
+        combobox_route.addItem("Schnell")
+            
+        
+    def edit_progressbar(self):
+        """
+        Edits the progressbar to show the progress.
+        """
+        current_value = self.calculation_progressbar.value()
+        maximum_value = self.calculation_progressbar.maximum()
+        self.calculation_progressbar.setValue \
+            (current_value + (maximum_value - current_value) / 100)
+        
+    def set_extension_page(self):
+        """
+        Sets the conditions of the options page in the tab structurizer.
+        """        
+        weather_information = QtWidgets.QTextEdit()
+        weather_information.setText \
+            ('Hier könnten Live Daten vom Wetter' 
+            '(weather.de) eingestellt werden.')
+        
+        news_field = QtWidgets.QTextEdit()
+        news_field.setText \
+            ('Hier könnten Live Nachrichten'
+            '(web.de) eingestellt werden')
+            
+        travel_helper = QtWidgets.QTextEdit()
+        travel_helper.setText \
+            ('Hier könnte Angebote von Booking'
+            '(booking.com) oder so reingebracht werden')
+            
+        troll_helper = QtWidgets.QTextEdit()
+        troll_helper.setText \
+            ('Hier könnte man eine Verbindung zu Bahn.de machen')
+
+        sublayout = QtWidgets.QVBoxLayout()
+        self.function_page.setLayout(sublayout)
+        sublayout.addWidget(weather_information)
+        sublayout.addWidget(news_field)
+        sublayout.addWidget(travel_helper)
+        sublayout.addWidget(troll_helper)
